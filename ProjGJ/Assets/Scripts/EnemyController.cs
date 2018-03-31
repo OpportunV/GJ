fac4c7db7cce6186cbbf6e2bb5d599ef5655a5ee
Vadim Ussik <Vadya.95@mail.ru>
2018-03-31 22:16:01 +0300

    j

---
+++ ProjGJ/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public Transform[] waypoints;
+    public GameObject weapon;
+
+    private Transform targetWaypoint;
+    private int targetIndex;
+
+	void Start () {
+        targetWaypoint = waypoints[1];
+	}
+	
+	void Update () {
+		
+	}
+}
