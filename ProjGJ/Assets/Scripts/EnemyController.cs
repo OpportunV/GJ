6ea4d87be8ff2df40e4335951eb930cda35ba0dc
Vadim Ussik <Vadya.95@mail.ru>
2018-03-31 22:43:46 +0300

    1223

--- ProjGJ/Assets/Scripts/EnemyController.cs
+++ ProjGJ/Assets/Scripts/EnemyController.cs
@@ -8,7 +8,7 @@ public class EnemyController : MonoBehaviour {
     public GameObject weapon;
     public float speed = 10f;
 
-    private Transform targetWaypoint;
+    private Vector3 targetWaypoint;
     private int targetIndex = 0;
 
 	void Start () {
@@ -18,18 +18,17 @@ public class EnemyController : MonoBehaviour {
 	
 	IEnumerator Patrol(float delay) {
         while (true) {
-            if (Mathf.Abs(transform.position.x - targetWaypoint.position.x) < 0.1f) {
+            if (Mathf.Abs(transform.position.x - targetWaypoint.x) < 0.1f) {
                 GetNextWaypoint();
+                yield return new WaitForSeconds(5 * delay);
             }
-            transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, speed * Time.fixedDeltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, speed * Time.fixedDeltaTime);
             yield return new WaitForSeconds(delay);
         }
     }
 
     void GetNextWaypoint() {
         targetIndex = (targetIndex + 1) % waypoints.Length;
-        targetWaypoint = waypoints[targetIndex];
-        targetWaypoint.position = new Vector2(waypoints[targetIndex].position.x, transform.position.y);
-
+        targetWaypoint = new Vector2(waypoints[targetIndex].position.x, transform.position.y);
     }
 }
