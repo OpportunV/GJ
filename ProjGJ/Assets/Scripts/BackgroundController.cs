fa96793880df491d801475f10ff8f428e4d16553
Vadim Ussik <Vadya.95@mail.ru>
2018-04-01 09:56:52 +0300

    s

---
+++ ProjGJ/Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+    public float parralax;
+    private MeshRenderer mr;
+    private Material material;
+    private Vector2 offset;
+
+    void Start() {
+        mr = GetComponent<MeshRenderer>();
+        material = mr.material;
+    }
+
+    void LateUpdate() {
+        offset.x = transform.position.x / parralax;
+        offset.y = transform.position.y / parralax;
+
+        material.mainTextureOffset = offset;
+    }
+}
